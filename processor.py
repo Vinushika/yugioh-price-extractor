933d5720eeb1ad71c25a125ae3135d8692e88e3e
Vinushika <stefan.schalk12@ncf.edu>
2015-05-20 15:39:09 -0400

    Added error conditions.

--- processor.py
+++ processor.py
@@ -10,6 +10,10 @@ input_file = values['input_file']
 ##now load the DB (important! maybe add as an argument?) and parse the image
 text_extractor.get_list_from_db("cards.cdb")
 card_name = text_extractor.extract_text(input_file)
+##If we don't have a card name, make sure we check for that
 ##and fetch price
-price = pricecrawler.get_price(card_name)
-print "Your card is %s and can be bought for %s at Trollandtoad.com" % (card_name, price)
+if card_name != None:
+	price = pricecrawler.get_price(card_name)
+	print "Your card is %s and can be bought for %s at Trollandtoad.com" % (card_name, price)
+else:
+	print "We couldn't get your card. Maybe try taking another picture?"
